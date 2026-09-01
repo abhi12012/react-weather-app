144728eb41bfe36c8db1bad99817b4ead8732abd
abhi12012 <agrawalabhishek211@gmail.com>
2026-09-01 11:17:44 +0530

    Start custom weather hook

---
+++ src/utils/weatherConditions.js
@@ -0,0 +1,32 @@
+const weatherConditions = {
+
+  0: "Clear Sky",
+  1: "Mainly Clear",
+  2: "Partly Cloudy",
+  3: "Overcast",
+  45: "Fog",
+  48: "Fog",
+  51: "Drizzle",
+  53: "Drizzle",
+  55: "Drizzle",
+  56: "Freezing Drizzle",
+  57: "Freezing Drizzle",
+  61: "Rain",
+  63: "Rain",
+  65: "Heavy Rain",
+  66: "Freezing Rain",
+  67: "Heavy Freezing Rain",
+  71: "Snow",
+  73: "Snow",
+  75: "Heavy Snow",
+  77: "Snow Grains",
+  80: "Rain Showers",
+  81: "Rain Showers",
+  82: "Heavy Rain Showers",
+  95: "Thunderstorm",
+  96: "Thunderstorm with Hail",
+  99: "Thunderstorm with Hail",
+
+};
+
+export default weatherConditions;

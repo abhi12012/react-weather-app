b8959c482ad25e611de22c81189aa02637ee52a3
abhi12012 <agrawalabhishek211@gmail.com>
2026-09-03 19:32:48 +0530

    Create context and wrap app with provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import WeatherContext from "./context/WeatherContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WeatherContext.Provider value="Hello Weather">
+  <App />
+</WeatherContext.Provider>
   </StrictMode>,
 )
